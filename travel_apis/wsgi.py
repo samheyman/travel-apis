75243a5bb7f4432aa799470fdb2bee7160c7a1db
Sam Heyman <sam.heyman@gmail.com>
2018-04-01 15:29:31 +0200

    fixing wsgi file

--- travel_apis/wsgi.py
+++ travel_apis/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "travel_apis.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "travel_apis.settings")
+os.environ['SETTINGS_CONFIG'] = 'travel_apis.settings'
 
 application = get_wsgi_application()
