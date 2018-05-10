516a6156d62ee70b97333d4793022e3e5de6629a
Sam Heyman <sam.heyman@gmail.com>
2018-05-10 12:40:50 +0200

    styling the destinations form

--- destinations/forms.py
+++ destinations/forms.py
@@ -3,6 +3,6 @@ from django import forms
 
 class LocationSearchForm(forms.Form):
     location = forms.CharField(label='Location', max_length=100, initial='Madrid')
-    number_of_results = forms.CharField(label='Number of results', max_length=3, initial=10)
-    provider = forms.ChoiceField(choices=[('yapq','YapQ'), ('avuxi', 'Avuxi')])
+    number_of_results = forms.CharField(label='Number of results', max_length=100, initial=10)
+    provider = forms.ChoiceField(choices=([('yapq','YapQ'), ('avuxi', 'Avuxi')]),widget=forms.Select(attrs={"class":"form-control form-control-sm", "style":"height:26px"}))
     
